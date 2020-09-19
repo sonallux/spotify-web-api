c12fb8cd27adf6ac11f393df3e170bbe5d96c2c4
Jonas <jsone-studios@users.noreply.github.com>
2020-09-19 16:58:21 +0200

    Add CLI to generator-ts (#12)

---
+++ scripts/generate-ts.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# This script runs the spotify-web-api-generator-ts to generate the TypeScript model definitions.
+# This script must be run from the root directory.
+
+API_DOCUMENTATION_FILE=./spotify-web-api-parser/api-documentation.yaml
+OUTPUT_FILE=./spotify-web-api-generator-ts/models.d.ts
+
+# Find a '*.cli-jar'
+cliJarFile=$(find ./spotify-web-api-generator-ts/target -maxdepth 1 -type f -name "*-cli.jar" -print -quit)
+
+if [ -z "$cliJarFile" ]
+then
+  echo "Unable to find generator-ts cli jar file. Did you run './mvnw package'?"
+  exit 1
+fi
+
+java -jar $cliJarFile -f $API_DOCUMENTATION_FILE -o $OUTPUT_FILE
+generationExitCode=$?
+if [ "$generationExitCode" != "0" ]
+then
+  echo "Generation failed. aborting..."
+  exit $generationExitCode
+fi
+
+exit 0
---
+++ spotify-web-api-generator-ts/src/main/java/de/jsone_studios/spotify/generator/ts/CLI.java
@@ -0,0 +1,51 @@
+package de.jsone_studios.spotify.generator.ts;
+
+import de.jsone_studios.spotify.parser.Yaml;
+import de.jsone_studios.spotify.parser.model.SpotifyApiDocumentation;
+import picocli.CommandLine;
+import picocli.CommandLine.Option;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class CLI implements Runnable {
+
+    @Option(names = {"-f", "--file"}, required = true, description = "The web API documentation file to a generate a Java wrapper for")
+    Path apiDocumentationFile;
+
+    @Option(names = {"-o", "--output"}, required = true, description = "The file the TypeScript model definitions should be written to")
+    Path outputFile;
+
+    @Option(names = { "-h", "--help" }, usageHelp = true, description = "display a help message")
+    boolean helpRequested = false;
+
+    @Override
+    public void run() {
+        SpotifyApiDocumentation apiDocumentation = readApiDocumentation();
+
+        try {
+            var tsGenerator = new TSGenerator();
+            tsGenerator.generate(apiDocumentation, outputFile);
+        } catch (IOException e) {
+            System.err.println("Failed to write generated files: " + e.getMessage());
+            System.exit(1);
+        }
+    }
+
+    private SpotifyApiDocumentation readApiDocumentation() {
+        SpotifyApiDocumentation apiDocumentation = null;
+        try (var inputStream = Files.newInputStream(apiDocumentationFile)) {
+            apiDocumentation = Yaml.create().readValue(inputStream, SpotifyApiDocumentation.class);
+        } catch (IOException e) {
+            System.err.println("Failed to read web API documentation file: " + e.getMessage());
+            System.exit(1);
+        }
+        return apiDocumentation;
+    }
+
+    public static void main(String[] args) {
+        var exitCode = new CommandLine(new CLI()).execute(args);
+        System.exit(exitCode);
+    }
+}
