f29d6f057b8083ad17d20019085cbd7c83809830
sonallux <13821543+sonallux@users.noreply.github.com>
2021-08-24 07:26:40 +0200

    Fix patch loading from jar file

--- spotify-web-api-parser/src/main/java/de/sonallux/spotify/parser/ApiPatches.java
+++ spotify-web-api-parser/src/main/java/de/sonallux/spotify/parser/ApiPatches.java
@@ -11,49 +11,73 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Objects;
-import java.util.stream.Stream;
+import java.util.Map;
+import java.util.function.Consumer;
 
 @Slf4j
 public class ApiPatches {
     private final ObjectMapper mapper;
+
+    private JsonNode webApiAsJsonNode;
 
     public ApiPatches() {
         mapper = Yaml.create();
     }
 
     public SpotifyWebApi applyPatches(SpotifyWebApi spotifyWebApi) throws ApiParseException {
-        JsonNode apiAsJsonNode = mapper.valueToTree(spotifyWebApi);
+        webApiAsJsonNode = mapper.valueToTree(spotifyWebApi);
 
-        var iterator = getPathsOfPatches().iterator();
-        while (iterator.hasNext()) {
-            var path = iterator.next();
-
-            try {
-                var patch = loadPatch(path);
-                apiAsJsonNode = patch.apply(apiAsJsonNode);
-            } catch (IOException e) {
-                log.error("Failed to load patch from path " + path, e);
-            } catch (PatchException e) {
-                log.warn("Failed to apply patch: " + path.getFileName(), e);
-            }
-        }
+        walkPatches(this::applyPatch);
 
         try {
-            return mapper.treeToValue(apiAsJsonNode, SpotifyWebApi.class);
+            return mapper.treeToValue(webApiAsJsonNode, SpotifyWebApi.class);
         } catch (JsonProcessingException e) {
             throw new ApiParseException("Result of patching is not a SpotifyWebApi", e);
         }
     }
 
-    private Stream<Path> getPathsOfPatches() throws ApiParseException {
+    private void applyPatch(Path path) {
         try {
-            var patchesDir = Path.of(ApiPatches.class.getResource("/patches/").toURI());
-            return Files.list(patchesDir);
+            var patch = loadPatch(path);
+            webApiAsJsonNode = patch.apply(webApiAsJsonNode);
+        } catch (IOException e) {
+            log.error("Failed to load patch from path " + path, e);
+        } catch (PatchException e) {
+            log.warn("Failed to apply patch: " + path.getFileName(), e);
+        }
+    }
+
+    private void walkPatches(Consumer<Path> patchConsumer) throws ApiParseException {
+        var patchesDirectory = "/patches/";
+        var resource = ApiPatches.class.getResource(patchesDirectory);
+        if (resource == null) {
+            throw new ApiParseException("Can not get patches resource");
+        }
+
+        FileSystem fs = null;
+        try {
+            Path path;
+            var uri = resource.toURI();
+            if ("jar".equals(uri.getScheme())) {
+                fs = FileSystems.newFileSystem(uri, Map.of());
+                path = fs.getPath(patchesDirectory);
+            } else {
+                path = Path.of(uri);
+            }
+            Files.list(path).forEach(patchConsumer);
         } catch (IOException | URISyntaxException e) {
             throw new ApiParseException("Failed to load patches", e);
+        } finally {
+            if (fs != null) {
+                try {
+                    fs.close();
+                } catch (IOException ignore) {
+                }
+            }
         }
     }
 
