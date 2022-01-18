133fe693b43d35561c4504afd0b723220680deb0
sonallux <13821543+sonallux@users.noreply.github.com>
2022-01-18 08:18:17 +0100

    Fix module-info.java files

---
+++ json-utils/src/moditect/module-info.java
@@ -0,0 +1,8 @@
+module de.sonallux.json {
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
+    requires com.jayway.jsonpath;
+    requires static lombok;
+
+    exports de.sonallux.json;
+}
