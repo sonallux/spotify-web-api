acfe97df76150d8bb3fe7f4c91f5311df3620d1a
sonallux <13821543+sonallux@users.noreply.github.com>
2022-01-25 21:45:44 +0100

    Add spotify-web-api-open-api module

---
+++ spotify-web-api-open-api/src/main/java/de/sonallux/spotify/openapi/DiffTest.java
@@ -0,0 +1,30 @@
+package de.sonallux.spotify.openapi;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
+import com.google.common.collect.Lists;
+import de.sonallux.json.patching.Patch;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
+
+public class DiffTest {
+    private static final ObjectMapper OPEN_API_YAML = io.swagger.v3.core.util.Yaml.mapper();
+
+    private static final Path OFFICIAL_OPENAPI_FILE = Path.of("official-spotify-open-api.yml");
+    private static final Path OFFICIAL_OPENAPI_FIXED_FILE = Path.of("spotify-web-api-open-api/spotify-web-api-openapi.yml");
+
+    public static void main(String[] args) throws Exception {
+        var officialOpenApi = OPEN_API_YAML.readTree(Files.readString(OFFICIAL_OPENAPI_FILE));
+
+        var openApiPatches = new OpenApiPatches();
+        var fixedOfficialOpenApi = openApiPatches.applyPatches(officialOpenApi);
+
+
+        var openApiAsString = OPEN_API_YAML.writeValueAsString(fixedOfficialOpenApi);
+        Files.writeString(OFFICIAL_OPENAPI_FIXED_FILE, openApiAsString);
+    }
+}
