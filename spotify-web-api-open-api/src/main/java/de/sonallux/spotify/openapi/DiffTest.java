a033b0703d0f902fbe2237f91cea1673ea566579
sonallux <13821543+sonallux@users.noreply.github.com>
2022-01-30 09:52:36 +0100

    Add validation with OpenAPI json schema

--- spotify-web-api-open-api/src/main/java/de/sonallux/spotify/openapi/DiffTest.java
+++ spotify-web-api-open-api/src/main/java/de/sonallux/spotify/openapi/DiffTest.java
@@ -1,12 +1,16 @@
 package de.sonallux.spotify.openapi;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.networknt.schema.*;
+import io.swagger.v3.core.util.Yaml;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class DiffTest {
-    private static final ObjectMapper OPEN_API_YAML = io.swagger.v3.core.util.Yaml.mapper();
+    private static final ObjectMapper OPEN_API_YAML = Yaml.mapper();
 
     private static final Path OFFICIAL_OPENAPI_FILE = Path.of("official-spotify-open-api.yml");
     private static final Path OFFICIAL_OPENAPI_FIXED_FILE = Path.of("spotify-web-api-open-api/spotify-web-api-openapi.yml");
@@ -17,8 +21,27 @@ public class DiffTest {
         var openApiPatches = new OpenApiPatches();
         var fixedOfficialOpenApi = openApiPatches.applyPatches(officialOpenApi);
 
+        validateOpenAPI(fixedOfficialOpenApi);
 
         var openApiAsString = OPEN_API_YAML.writeValueAsString(fixedOfficialOpenApi);
         Files.writeString(OFFICIAL_OPENAPI_FIXED_FILE, openApiAsString);
+    }
+
+    private static void validateOpenAPI(JsonNode node) throws IOException {
+        var openApiJsonSchema = loadOpenApiSchema();
+
+        var schemaFactory = JsonSchemaFactory.getInstance(SpecVersionDetector.detect(openApiJsonSchema));
+        var schema = schemaFactory.getSchema(openApiJsonSchema);
+
+        var validationMessages = schema.validate(node);
+        for (var msg : validationMessages) {
+            System.err.println(msg);
+        }
+
+    }
+
+    private static JsonNode loadOpenApiSchema() throws IOException {
+        var mapper = new ObjectMapper();
+        return mapper.readTree(DiffTest.class.getResourceAsStream("/open-api-spec-schema.json"));
     }
 }
