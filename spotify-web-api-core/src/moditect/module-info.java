ae13a522a181866893546c50a9204976a6fb154e
sonallux <13821543+sonallux@users.noreply.github.com>
2022-02-06 17:57:27 +0100

    Remove old parser and generators

--- spotify-web-api-core/src/moditect/module-info.java
+++
@@ -1,9 +0,0 @@
-module de.sonallux.spotify.core {
-    requires com.fasterxml.jackson.dataformat.yaml;
-    requires com.fasterxml.jackson.annotation;
-    requires com.fasterxml.jackson.databind;
-    requires static lombok;
-
-    exports de.sonallux.spotify.core;
-    exports de.sonallux.spotify.core.model;
-}
